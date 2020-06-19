1908200b9856870a37554c84a6c888a00cf76b94
Samuel Soares <34168199+samuelsoares27@users.noreply.github.com>
2020-06-19 16:53:35 -0300

    Update server.js

--- Server.js
+++ Server.js
@@ -1,11 +1,21 @@
 var express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
 var porta = 3000;
 var app = express();
 
-app.get('/', function (req, res) {
-    res.json({ status: 'Servidor rodando!' })
-})
+
+app.use(cors())
+app.use(bodyParser.json());
+
 
 app.listen(porta, function () {
     console.log('Servidor está rodando no localhost:' + porta)
 })
+
+// Método get chamado por Localhot:3000
+app.get('/', function (req, res) {
+    res.json({ status: 'Servidor rodando!' })
+})
+
+
